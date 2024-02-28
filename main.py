84de6a6ae549f4c3491ac51e15d9e2aa1de57f09
Nino Ross <9670771+Chibdibs@users.noreply.github.com>
2024-02-28 10:52:03 +0900

    Included the api key in pycharm main.py environment variables. Cleaned up the code and installed python-multipart And changed the host from 0.0.0.0 to 127.0.0.1

--- main.py
+++ main.py
@@ -98,4 +98,4 @@ async def transcribe_audio_file(file: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
